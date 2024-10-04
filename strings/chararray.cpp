894942b5fece7039a80a45fb07b1f7e49752dd9d
Vighnesh Shukla <vighneshshukla00@gmail.com>
2024-10-04 20:02:25 +0530

    Add files via upload

---
+++ strings/chararray.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+
+
+    char name[58];
+    cout<<"Enter the number:-";
+    cin >> name;
+   cout << "Your name is "<< name<<endl;
+}
