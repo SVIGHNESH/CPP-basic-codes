f20a8511bb933344fe5a5052070355a8ee507d6a
VIGHNESH <vighneshshukla00@gmail.com>
2024-11-16 18:55:18 +0530

    added the print statement in between the calls to to visualize how swapping is taking place

--- Recursion/String_with_Recursion.cpp/strings_1.cpp
+++ Recursion/String_with_Recursion.cpp/strings_1.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 void reverse(int i,int j,string &str){
+
+
+    cout<<"Call recieved for  "<<str<<endl;
     //base case 
-    if(i>j){
+    if(i>j)
         return;
-    }
+    
     swap(str[i],str[j]);
     i++;
     j--;
@@ -22,6 +25,9 @@ int main(){
     cin>>str;
     int i=0;
     int j=str.length()-1;
+    cout<<endl;
     reverse(i,j,str);
+    cout<<endl;
     cout<<str;
+    
 }
