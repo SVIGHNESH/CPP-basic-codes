009c8c9cb718408c14cfc10db36d91aafeca6770
VIGHNESH <vighneshshukla00@gmail.com>
2024-11-26 10:48:43 +0530

    selectionsort

--- Algos/SORTING/selectionsort.cpp
+++ Algos/SORTING/selectionsort.cpp
@@ -1,4 +1,7 @@
-void selectionSort(vector<int>& arr, int n)
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n)
 {
   for (int i = 0; i < n - 1; i++) {
     int minIndex = i;
@@ -10,4 +13,19 @@ void selectionSort(vector<int>& arr, int n)
    swap(arr[minIndex],arr[i]);
    }
     
+}
+int main (){
+  int arr[5]={12,23,2445,212,2};
+  int n =5  ;
+  cout << "Unsorted Array:" << endl;
+  
+  for(int i = 0 ; i< n;i++){
+    cout << arr[i] << " ";
+  }cout << endl;
+  selectionSort(arr,n);
+  cout << "Sorted Array:" << endl;
+  
+  for(int i = 0 ; i< n;i++){
+    cout << arr[i] << " ";
+  }
 }
