60590d208d9b4051ea4ecdbba1a6d58427a32193
VIGHNESH <vighneshshukla00@gmail.com>
2024-12-03 22:22:30 +0530

    made a hero.cpp named file in the same directory as the executable and added the following code to it:

---
+++ OOPS/hero_class.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+
+class Hero{
+
+   private:
+      //properties
+      int Health ;
+   public:
+      char level ;
+
+
+      int  getHealth(){
+        return Health;
+      }
+
+      char getLevel(){
+        return level ;
+      }
+
+      void setHealth(int h ){
+        Health = h ;
+
+      }
+
+      void setLevel(char ch ){
+                level = ch;
+
+      }
+
+
+};
+
+int   main (){
+
+    //creation of the object
+
+
+    //static allocation
+    Hero a;
+    cout << "Health of a is " << a.getHealth() << endl ;
+    cout << "Level of a is " << a.getLevel() << endl ;
+
+
+    //dynamically
+    Hero *b = new Hero;
+    cout << "Health of a is " << (*b).getHealth() << endl ;
+    cout << "Level of a is " << (*b).getLevel() << endl ;
+
+
+
+    // cout << "The Size of the object is " << sizeof(Ramesh) << endl;
+
+
+    // //using the getter
+    // cout << "Ramesh Health is:" << Ramesh.getHealth() << endl;
+    // Ramesh.level = 'A';
+
+    // // using the Setter
+
+    // Ramesh.setHealth(78);
+    // cout << "The Level of Ramesh is " <<  Ramesh.getHealth() <<endl;
+    // //cout << "Size of the Hero:" << sizeof(h1) << endl;
+
+
+}
