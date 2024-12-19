d2944e066a6cf9732e1af9265dcbef97826f09bf
VIGHNESH <vighneshshukla00@gmail.com>
2024-12-19 21:57:53 +0530

    i don't know what i am commiting

--- Recursion/Fibonacci_nth.cpp
+++ Recursion/Fibonacci_nth.cpp
@@ -21,3 +21,22 @@ int main (){
     print = fib(n);
     cout << print << endl;
 }
+
+class MyClass {
+public:
+  MyClass();
+  MyClass(MyClass &&) = default;
+  MyClass(const MyClass &) = default;
+  MyClass &operator=(MyClass &&) = default;
+  MyClass &operator=(const MyClass &) = default;
+  ~MyClass();
+
+private:
+  
+};
+
+MyClass::MyClass() {
+}
+
+MyClass::~MyClass() {
+}
