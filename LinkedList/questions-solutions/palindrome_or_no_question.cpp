3b4912cb90b6b8b74d2d5d47f47e9a4f2f21d575
Vighnesh <vighneshshukla00@gmail.com>
2025-01-20 23:00:29 +0530

    second approach to the question LL is palindrome or not

--- LinkedList/questions-solutions/palindrome_or_no_question.cpp
+++ LinkedList/questions-solutions/palindrome_or_no_question.cpp
@@ -1,3 +1,38 @@
+//approach no. 1 
+
+    bool checkPalindrome(vector<int> arr){
+        int n = arr.size();
+        int s = 0;
+        int e = n - 1;
+        
+        while(s<=e){
+            if(arr[s] != arr[e]){
+                return false;
+                
+            }
+                s++;
+                e--;
+                
+            }
+            return 1;
+        }
+    
+  
+
+    // Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head) {
+            vector<int> arr;
+            Node* temp = head;
+            while(temp!= NULL){
+                arr.push_back(temp -> data);
+                temp = temp -> next ;
+                
+            }
+            return checkPalindrome(arr);
+    }
+
+//approach no. 2 
+
 private:
     bool checkPalindrome(vector<int> arr){
         int n = arr.size();
@@ -30,3 +65,5 @@ private:
             return checkPalindrome(arr);
     }
 };
+
+
