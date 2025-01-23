4b734a11297f9a274ad1c8e842fb4e50181e5c54
Vighnesh <vighneshshukla00@gmail.com>
2025-01-23 22:24:58 +0530

    created a stack

--- STL/stacckSTL.cpp
+++ STL/stacckSTL.cpp
@@ -1,14 +1,21 @@
-#include<iostream>
-#include<stack>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
     
-    stack<string> s;
-    s.push("VIGHNESH");
-    s.push("SHUKLA");
-    cout<<"Top element of the stack"<<s.top()<<endl;
+
+    //creation of stack
+    stack<int> s;
+
+    //push operation
+    s.push(34);
+    cout <<"Printing the Top Element:"<< s.top( ) << endl;
+
+    // stack<string> s;
+    // s.push("VIGHNESH");
+    // s.push("SHUKLA");
+    // cout<<"Top element of the stack"<<s.top()<<endl;
     
-    cout<<"THe size of the stack --> "<<s.size()<<endl;
-    cout<<"EMPTY OR NOT --> "<<s.empty()<<endl;
+    // cout<<"THe size of the stack --> "<<s.size()<<endl;
+    // cout<<"EMPTY OR NOT --> "<<s.empty()<<endl;
 
 }
