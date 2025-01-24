d7681f1f1c4fac80def60a409df7a79479f7007c
Vighnesh <vighneshshukla00@gmail.com>
2025-01-24 21:02:45 +0530

    stack implementation

--- STL/stacckSTL.cpp
+++ STL/stacckSTL.cpp
@@ -1,14 +1,79 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+class Stack{
+    //properties
+
+    int *arr;
+    int top;
+    int size;
+
+
+
+
+
+    //behaviour
+    Stack(int size){
+        this -> size = size;
+        arr = new int [size];
+        top = -1;
+
+    }
+
+    void push(int element){
+            if(size - top > 1){
+                top++;
+                arr[top] = element;
+
+            }
+            else{
+                cout << "Stack OverFlow" << endl;
+            }
+    }
+
+    void pop(){
+
+        if(top >= 0){
+            top--;
+
+        }
+        else{
+            cout << "Stack UnderFlow" << endl;
+        }
+    }
+
+    int peek(){
+        if(top >= 0 && top < size)
+            return arr[top];
+        else{
+            cout << "Stack is Empty" << endl; 
+
+        }
+    }
+
+    bool isEmpty(){
+
+    }
+};
 int main(){
     
-
+/*
     //creation of stack
     stack<int> s;
 
     //push operation
     s.push(34);
     cout <<"Printing the Top Element:"<< s.top( ) << endl;
+    // s.pop();
+
+    if(s.empty()){
+        cout << "Stack is Empty" << endl;
+    }
+    else{
+        cout << s.top();
+    }
+
+    cout << "The size of the stack is "<< s.size();
 
     // stack<string> s;
     // s.push("VIGHNESH");
@@ -17,5 +82,7 @@ int main(){
     
     // cout<<"THe size of the stack --> "<<s.size()<<endl;
     // cout<<"EMPTY OR NOT --> "<<s.empty()<<endl;
+*/
+
 
 }
