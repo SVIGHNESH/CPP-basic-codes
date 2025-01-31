70fbc1888b03306a01ad733ab61442a597131eb2
Vighnesh <vighneshshukla00@gmail.com>
2025-01-31 22:04:27 +0530

    next

--- Queue/queueBySTL.cpp
+++ Queue/queueBySTL.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(){
     queue<int>q;
     q.push(33);
-    
+    q.push(3456);//vighneshshukla00@gmail.com
     cout<<"The size of the queue is :" << q.size() << endl;
 }
