2ef9505a0a8988a1dd9c8c8824631d559b3f0cb7
Vighnesh <vighneshshukla00@gmail.com>
2025-02-05 23:18:08 +0530

    xghew

--- array/arrayscope.cpp
+++ array/arrayscope.cpp
@@ -14,7 +14,7 @@ void update(int arr[],int n){
 int main (){
     int arr[3]={1,2,3};
     update(arr,3);
-    for(int i = 0;i<3;i++){
+    for(int i = 0;i<3;i++){//sryhyfuyl;u
         cout<<arr[i]<<endl;
     }
 
