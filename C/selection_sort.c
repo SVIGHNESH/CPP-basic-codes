5d2425c72bc6682394af8172fe3f534a59c03c33
Vighnesh <vighneshshukla00@gmail.com>
2025-02-28 18:48:08 +0530

    etrthdfdn

--- C/selection_sort.c
+++ C/selection_sort.c
@@ -8,11 +8,23 @@ void swap(int *a,int *b){
 void selectionSort(int arr[],int n){
     for(int i = 0 ; i < n ; i++){
         int minIndex = i;
-        for (int j = i+1;j<n;i++){
+        for (int j = i+1;j<n;j++){
             if(arr[j] < arr[minIndex]){
                 minIndex = j;
             }
-            swap(&arr[i],arr[minIndex]);
+            swap(&arr[i],&arr[minIndex]);
         }
     }
+    
+}
+
+int main(){
+    int arr[5]={5,4,3,2,1};
+    int n = 5;
+    selectionSort(arr,n);
+    for(int i = 0 ; i< n ;i++){
+        printf("%d ",arr[i]);
+    }
+   return 0;
+    
 }
